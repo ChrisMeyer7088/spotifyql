ae8ae5f39aa52584e7fc0ac9edecec826b624ceb
Chris Meyer <chris_meyer_2@hotmail.com>
2021-03-21 22:12:57 -0700

    Added markets query (#6)
    * Added markets query
    
    * Added missing line

--- backend/src/graphql/resolvers/root.js
+++ backend/src/graphql/resolvers/root.js
@@ -1,11 +1,13 @@
 const artist = require('./artist')
 const album = require('./album')
 const track = require('./track')
+const market = require('./market')
 
 const root = {}
 
 Object.assign(root, artist)
 Object.assign(root, album)
 Object.assign(root, track)
+Object.assign(root, market)
 
 module.exports = root
